4fb8fbc7cf26c512d8aa462977f953bbb3ac36c9
antonionovelli92 <anto.novelli@hotmail.it>
2022-12-14 14:51:40 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*
+Esercizio di oggi: Lista della spesa con ciclo while
+nome repo: js-lista-spesa
+Consegna:
+Data una lista della spesa (inventatela), stampare in pagina gli elementi della lista individualmente con un ciclo while.  (anche brutalmente, basta che si vedano)
+Bonus:
+Provare a stampare sia con i template literals che con i nodi, manipolando il DOM.
+Consigli:
+- Ricordiamoci di inizializzare le variabili  che servono alla condizione FUORI dal ciclo while, altrimenti verranno azzerate ad ogni ciclo
+- Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
+*/
+
+const shop = ['Pomodoro', 'Latte', 'Pane', 'Carne'];
